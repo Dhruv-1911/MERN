471ebc00443632b86ead1d1252ed676f8d4eadb7
Dhruv-1911 <dhruvk@zignuts.com>
2023-08-07 11:43:58 +0530

    list product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header></header>
+      <main>
+        <h1>Featured Product</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="productInfo">
+                <a href={`/product/${product.slug}`}>
+                  <h5>{product.name}</h5>
+                </a>
+                <h5><b>${product.price}</b></h5>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
