4b8c80b97926a2f59c4acd39ede21f2795763d2e
Dhruv-1911 <dhruvk@zignuts.com>
2023-08-18 13:15:03 +0530

    change in server

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,14 @@ app.use('/api/user', userrouter);
 app.use('/api/order', orderrouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname + '/frontend/build')));
+app.use(express.static(path.join(__dirname , '/frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname , '/frontend/build/index.html'));
 });
+
+
+
 
 app.use((err, req, res, next) => {
   console.log('err: ', err);
